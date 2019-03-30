1321ce2adbf1a2ece30d4b42a34d8ab5aa1c3ce5
gdarlan <gabriel.darlan@hotmail.com>
2019-03-30 13:02:45 -0300

    arquivos da aula 5

---
+++ financas/src/br/com/caelum/financas/teste/TestaContaCliente.java
@@ -0,0 +1,12 @@
+package br.com.caelum.financas.teste;
+
+import br.com.caelum.financas.modelo.Cliente;
+
+public class TestaContaCliente {
+
+	public static void main(String[] args) {
+		Cliente cliente=new Cliente();
+		cliente.setNome("Gabrie");
+		cliente.setEndereco("rua nena");
+	}
+}
